c038067e3c2d97778df943b023fa2a54bc335a9d
cdelacombaz <cdelacombaz@bluewin.ch>
2023-04-06 11:15:23 +0200

    Added useEffect

--- src/components/ContactBook/index.jsx
+++ src/components/ContactBook/index.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './styles.css'
 import Contact from '../Contact'
 import AddContact from '../AddContact'
+import { initialContacts } from './constants'
 
 const ContactBook = () => {
   const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    // here, an API call would happen and the response would be the inital state
+    // to simulate it, we use some hardcoded values
+    setContacts(initialContacts)
+  }, [])
 
   return (
     <main className="contact-book-container">
